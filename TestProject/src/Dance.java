472f1c4bd981a37c1699b7c10d247176461b16af
P.Million <pawloszeyede21@augustana.edu>
2022-09-28 01:16:04 -0500

    Added dancing and saying goodbye.

---
+++ TestProject/src/Dance.java
@@ -0,0 +1,6 @@
+
+public class Dance {
+	public static void main(String[] args) {
+		System.out.println("Watch those chickens dance!");
+	}
+}
--- TestProject/src/TestMain.java
+++ TestProject/src/TestMain.java
@@ -3,5 +3,6 @@ public class TestMain {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello Chicken World!");
+		System.out.println("Goodbye");
 	}
 }
